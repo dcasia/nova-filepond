028d9b51a95c96b95b827e4476a7a35a23d31ad1
Rafael Milewski <rafael.milewski@gmail.com>
2023-09-19 14:53:55 +0800

    update readme

--- config/nova-filepond.php
+++ config/nova-filepond.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 return [
 
-    'disk' => config('nova.storage_disk', 'public'),
     'temp_disk' => 'local',
     'temp_path' => 'nova-filepond/temp',
 
--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,6 @@ use DigitalCreative\Filepond\Http\Controllers\ProcessController;
 use DigitalCreative\Filepond\Http\Controllers\RevertController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Tool API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you may register API routes for your tool. These routes
-| are loaded by the ServiceProvider of your tool. You're free to add
-| as many additional routes to this file as your tool may require.
-|
- */
-
+Route::get('/load', LoadController::class)->name('nova.filepond.load');
 Route::post('/process', ProcessController::class)->name('nova.filepond.process');
 Route::delete('/revert', RevertController::class)->name('nova.filepond.revert');
-Route::get('/load', LoadController::class)->name('nova.filepond.load');
